d7d78ba48da5a9c135f9d48fdba5963cff957e4b
Verchenok <verailinskay@gmail.com>
2021-02-22 15:27:44 +0300

    Five_tasks

---
+++ src/Five.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+public class Five {
+    public static void main(String[] args) {
+        Scanner in = new Scanner (System.in);
+        System.out.print("Пожалуйста, введите первое логическое значение: ");
+        boolean a = in.nextBoolean();
+        in = new Scanner (System.in);
+        System.out.print("Пожалуйста, введите второе логическое значение: ");
+        boolean b = in.nextBoolean();
+        boolean result = and(a,b);
+        System.out.printf("Результат: %s", result);
+    }
+
+    private static boolean and(boolean a, boolean b) {
+        if (a == true && b == true) {
+            return true;
+        }
+        else {
+            return false;
+        }
+    }
+}
