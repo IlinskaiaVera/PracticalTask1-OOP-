bc23fe20d8c2b3c2c79bcc9c6cfbb49bba6af62b
Verchenok <verailinskay@gmail.com>
2021-02-22 21:00:26 +0300

    Seven_tasks

---
+++ src/Seven.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class Seven {
+    public static void main(String[] args) {
+       Scanner in = new Scanner(System.in);
+       System.out.print("Введите число, чтобы узнать, что делает данная функция: ");
+       int number = in.nextInt();
+       int result = squared(number);
+       System.out.printf("Это функция, которая возводит число в степень 2 :). И результат: %s", result);
+    }
+    private static int squared(int a) { //squared, а не squaed. (int a), а не (int b) -_-
+        return a*a;
+    }
+}
